d3c84407c305c3960798478417d3d958751b51cb
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2021-01-29 10:00:45 +0100

    Update fonctions_de_base.py

--- fonctions_de_base.py
+++ fonctions_de_base.py
@@ -9,16 +9,24 @@ import random as rd
 def cree_Carte(n=100,p=50):
   #les salles seront un nombre aléatoire entre 3 et 6
   #la taille des salles varie entre 4x4 et 6x10
+  M=np.zeros((n,p),dtype='string')
+  C=cases_occupes(M)
   nbreSalle=rd.randint(3,7)
   for i in range(nbreSalle):
     largeur=rd.randint(4,7)
     longueur=rd.randint(4,11)
     cree_salle(longueur,largeur)
 
+  return M
 
+def cases_occupes(M):
+  n,p=M.shape
+  C=np.zeros((n,p))
+  return C
 
 def cree_salle((n,p)):
-  M=np.zeros((n,p),dtype='object')
+  """crée une salle dans un endroit non occupé de la carte"""
+
 
 def dans_une_salle((i,j,k))
 
