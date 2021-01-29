39a6bd6c6a155b756c4ad6da5d68f8aa40e9afb6
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2021-01-29 09:51:40 +0100

    Update fonctions_de_base.py

--- fonctions_de_base.py
+++ fonctions_de_base.py
@@ -1,2 +1,29 @@
+import numpy as np
+import pygame
+
+
+#types de cellules et de méchants :
+#... à lister ...
+
+
 def mignot(n):
   return n*'mignot'
+
+
+def cree_Carte(nbrSalle):
+  #les salles seront un nombre aléatoire entre 3 et 6
+  #la taille des salles varie entre 4x4 et 6x10
+
+
+def cree_salle((n,p)):
+  M=np.zeros((n,p),dtype='object')
+
+def dans_une_salle((i,j,k))
+
+
+
+def cree_Passage(salle):
+  """ crée un passage sur les bords d'une salle"""
+
+
+def cree_Monstre()
